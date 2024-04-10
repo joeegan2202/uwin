c62e8073bfdb4a95e01d45f537fbf3e0f065af48
Joseph Egan <joe@eganshub.net>
2024-04-10 15:16:52 -0500

    Added nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Development environment";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+
+  outputs =
+    { self
+    , flake-utils
+    , nixpkgs
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            pkgs.clang
+          ];
+          buildInputs = with pkgs; [
+
+          ];
+        };
+      }
+    );
+}
